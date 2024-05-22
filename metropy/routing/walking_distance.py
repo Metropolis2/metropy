1ce625e7745e0d048bf309e623a771c075e58a8d
LucasJavaudin <lucas@lucasjavaudin.com>
2024-05-22 11:44:25 +0200

    Use distance to edge instead of node

--- metropy/routing/walking_distance.py
+++ metropy/routing/walking_distance.py
@@ -1,11 +1,16 @@
 import os
+import shutil
 import time
 import json
 import subprocess
 
+import numpy as np
 import polars as pl
 import geopandas as gpd
 from shapely.geometry import Point
+
+import metropy.utils.mpl as mpl
+
 
 def read_trips(input_directory: str):
     print("Reading trips' coordinates...")
@@ -24,12 +29,26 @@ def read_trips(input_directory: str):
         (pl.col("origin_lng") != pl.col("destination_lng"))
         | (pl.col("origin_lat") != pl.col("destination_lat"))
     )
-    return df
+    # Create a DataFrame will all the unique (lng, lat) pairs.
+    nodes = pl.concat(
+        (
+            df.select(pl.col("origin_lng").alias("lng"), pl.col("origin_lat").alias("lat")),
+            df.select(
+                pl.col("destination_lng").alias("lng"), pl.col("destination_lat").alias("lat")
+            ),
+        ),
+        how="vertical",
+    ).unique()
+    print(f"Number of unique origin / destination points: {len(nodes)}")
+    return df, nodes
 
 
 def read_edges(filename: str, crs: str, config: dict):
-    print("Reading edges")
-    gdf = metro_io.read_geodataframe(filename, columns=["id", "source", "target", "length"])
+    print("Reading edges...")
+    gdf = metro_io.read_geodataframe(
+        filename, columns=["id", "source", "target", "length", "road_type", "geometry"]
+    )
+    gdf.rename(columns={"id": "edge_id"}, inplace=True)
     gdf.to_crs(crs, inplace=True)
     if "forbidden_road_types" in config:
         assert isinstance(config["forbidden_road_types"], list)
@@ -39,105 +58,125 @@ def read_edges(filename: str, crs: str, config: dict):
     return gdf
 
 
-def find_origin_destination_node(trips, edges):
-    print("Finding closest edge to each origin")
-    # Create source point of each edge.
-    edges["source_point"] = edges["geometry"].apply(lambda g: Point(g.coords[0]))
-    # Find the closest edge to each origin, with the distance to the edge and to the source node.
-    origins = gpd.GeoDataFrame(
-        {"id": trips["trip_id"]},
-        geometry=gpd.points_from_xy(trips["origin_lng"], trips["origin_lat"], crs="EPSG:4326"),
+def find_origin_destination_node(nodes: pl.DataFrame, edges: gpd.GeoDataFrame):
+    print("Creating origin / destination points...")
+    # Create a GeoDataFrame of all the origin / destination nodes.
+    gdf = gpd.GeoDataFrame(
+        data={"lng": nodes["lng"], "lat": nodes["lat"]},
+        geometry=gpd.points_from_xy(nodes["lng"], nodes["lat"], crs="EPSG:4326"),
     )
-    origins.sjoin_nearest(
-        edges.loc[edges["allow_od"], ["source", "geometry", "source_point"]],
-        distance_col="origin_edge_dist",
+    gdf.to_crs(edges.crs, inplace=True)
+    print("Matching to the nearest edge...")
+    # Match to the nearest edge.
+    gdf = gdf.sjoin_nearest(
+        edges.loc[edges["allow_od"], ["edge_id", "geometry", "source", "target"]],
+        distance_col="edge_dist",
         how="left",
     )
     # Duplicate indices occur when there are two edges at the same distance.
-    origins = origins.drop_duplicates(subset=["trip_id"])
-    origins["origin_source_dist"] = origins.geometry.distance(origins["source_point"])
-    trips = trips.join(
-        pl.from_pandas(origins[["trip_id", "source", "origin_edge_dist", "origin_source_dist"]]),
-        on="trip_id",
-    ).rename({"source": "origin_node"})
-    # Compute the distance to the source node when traveling through the edge, using Pythagore
-    # Theorem.
-    trips = trips.with_columns(
-        ((pl.col("origin_source_dist") ** 2) - (pl.col("origin_edge_dist") ** 2))
-        .sqrt()
-        .alias("origin_source_dist_on_edge")
+    gdf.drop_duplicates(subset=["lng", "lat"], inplace=True)
+    print("Creating source / target points...")
+    # Create source / target point of the edges.
+    matched_edges = edges.loc[edges["edge_id"].isin(gdf["edge_id"]), ["edge_id", "geometry"]]
+    matched_edges["source_point"] = matched_edges["geometry"].apply(lambda g: Point(g.coords[0]))
+    matched_edges["target_point"] = matched_edges["geometry"].apply(lambda g: Point(g.coords[-1]))
+    gdf = gdf.merge(matched_edges[["edge_id", "source_point", "target_point"]], on="edge_id")
+    print("Computing distances to source / target nodes...")
+    # Compute distance to the source / target node of nearest edge.
+    gdf["source_dist"] = gdf["geometry"].distance(gdf["source_point"])
+    gdf["target_dist"] = gdf["geometry"].distance(gdf["target_point"])
+    print("Finding nearest node...")
+    # Set the nearest node.
+    df = pl.from_pandas(
+        gdf.loc[
+            :, ["lng", "lat", "edge_id", "edge_dist", "source", "target", "source_dist", "target_dist"]
+        ]
     )
-    print("Finding closest edge to each destination")
-    # Do the same for the destinations.
-    destinations = gpd.GeoDataFrame(
-        {"id": trips["trip_id"]},
-        geometry=gpd.points_from_xy(
-            trips["destination_lng"], trips["destination_lat"], crs="EPSG:4326"
-        ),
+    mask = df["source_dist"] > df["target_dist"]
+    df = df.with_columns(
+        pl.when(mask).then(pl.col("target")).otherwise(pl.col("source")).alias("node"),
+        pl.when(mask)
+        .then(pl.col("target_dist"))
+        .otherwise(pl.col("source_dist"))
+        .alias("node_dist"),
     )
-    destinations.sjoin_nearest(
-        edges.loc[edges["allow_od"], ["source", "geometry", "source_point"]],
-        distance_col="destination_edge_dist",
-        how="left",
-    )
-    destinations = destinations.drop_duplicates(subset=["trip_id"])
-    destinations["destination_source_dist"] = destinations.geometry.distance(
-        destinations["source_point"]
-    )
-    trips = trips.join(
-        pl.from_pandas(
-            destinations[["trip_id", "source", "destination_edge_dist", "destination_source_dist"]]
-        ),
-        on="trip_id",
-    ).rename({"source": "destination_node"})
-    trips = trips.with_columns(
-        ((pl.col("destination_source_dist") ** 2) - (pl.col("destination_edge_dist") ** 2))
-        .sqrt()
-        .alias("destination_source_dist_on_edge")
-    )
-    print("Number of unique origin nodes: {:,}".format(trips["origin_node"].n_unique()))
-    print("Number of unique destination nodes: {:,}".format(trips["destination_node"].n_unique()))
-    n = trips.n_unique(subset=["origin_node", "destination_node"])
-    print("Number of unique OD pairs: {:,}".format(n))
     print(
-        "Distance between origin point and start edge: {}".format(
-            trips["origin_edge_dist"].describe()
+        "Distance between origin / destination point and nearest edge: {}".format(
+            df["edge_dist"].describe()
         )
     )
     print(
-        "Distance between destination point and end edge: {}".format(
-            trips["destination_edge_dist"].describe()
+        "Distance between origin / destination point and nearest node: {}".format(
+            df["node_dist"].describe()
         )
     )
-    trips = trips.select(
-        "trip_id",
-        "origin_node",
-        "destination_node",
-        "origin_edge_dist",
-        "origin_source_dist",
-        "origin_source_dist_on_edge",
-        "destination_edge_dist",
-        "destination_source_dist",
-        "destination_source_dist_on_edge",
+    df = df.select(
+        "lng",
+        "lat",
+        "node",
+        "node_dist",
+        "edge_dist",
+    )
+    return df
+
+
+def add_od_to_trips(trips: pl.DataFrame, df: pl.DataFrame):
+    print("Assigning origin / destination node to all trips...")
+    lztrips = (
+        trips.lazy()
+        .join(df.lazy(), left_on=["origin_lng", "origin_lat"], right_on=["lng", "lat"])
+        .rename(
+            {
+                "node": "origin_node",
+                "node_dist": "origin_node_dist",
+                "edge_dist": "origin_edge_dist",
+            }
+        )
+    )
+    trips = (
+        lztrips.join(
+            df.lazy(), left_on=["destination_lng", "destination_lat"], right_on=["lng", "lat"]
+        )
+        .rename(
+            {
+                "node": "destination_node",
+                "node_dist": "destination_node_dist",
+                "edge_dist": "destination_edge_dist",
+            }
+        )
+        .select(
+            "trip_id",
+            "origin_node",
+            "origin_node_dist",
+            "origin_edge_dist",
+            "destination_node",
+            "destination_node_dist",
+            "destination_edge_dist",
+        )
+        .collect()
     )
     return trips
 
 
-def prepare_routing(trips: pl.DataFrame, edges, tmp_directory: str):
-    print("Saving queries")
+def prepare_routing(trips: pl.DataFrame, edges: gpd.GeoDataFrame, tmp_directory: str):
+    print("Saving queries...")
     queries = trips.unique(subset=["origin_node", "destination_node"]).select(
         pl.int_range(0, pl.len(), dtype=pl.UInt64).alias("query_id"),
-        pl.col("node_origin_walk").alias("origin"),
-        pl.col("node_destination_walk").alias("destination"),
+        pl.col("origin_node").alias("origin"),
+        pl.col("destination_node").alias("destination"),
         pl.lit(0.0).alias("departure_time"),
     )
+    print("Number of unique origin nodes: {:,}".format(queries["origin"].n_unique()))
+    print("Number of unique destination nodes: {:,}".format(queries["destination"].n_unique()))
+    print("Number of unique OD pairs: {:,}".format(len(queries)))
     queries.write_parquet(os.path.join(tmp_directory, "queries.parquet"))
-    print("Saving graph")
+    print("Saving graph...")
+    edges = pl.from_pandas(edges.loc[:, ["edge_id", "source", "target", "length"]])
     # Parallel edges are removed, keeping only the shortest edge.
-    edges.sort("length").unique(subset=["source", "target"], keep="first").sort("id").select(
-        pl.col("id").alias("edge_id"), "source", "target", pl.col("length").alias("travel_time")
+    edges.sort("length").unique(subset=["source", "target"], keep="first").sort("edge_id").select(
+        "edge_id", "source", "target", pl.col("length").alias("travel_time")
     ).write_parquet(os.path.join(tmp_directory, "edges.parquet"))
-    print("Saving parameters")
+    print("Saving parameters...")
     parameters = {
         "algorithm": "Best",
         "output_route": False,
@@ -145,7 +184,7 @@ def prepare_routing(trips: pl.DataFrame, edges, tmp_directory: str):
             "queries": "queries.parquet",
             "edges": "edges.parquet",
         },
-        "output_directory": os.path.join(tmp_directory, "output"),
+        "output_directory": "output",
         "saving_format": "Parquet",
     }
     with open(os.path.join(tmp_directory, "parameters.json"), "w") as f:
@@ -157,19 +196,19 @@ def run_routing(routing_exec: str, tmp_directory: str):
     # Create the routing command.
     parameters_filename = os.path.join(tmp_directory, "parameters.json")
     command = f"{routing_exec} {parameters_filename}"
-    print("Running routing")
+    print("Running routing...")
     subprocess.run(command, shell=True)
 
 
 def load_results(tmp_directory: str):
-    print("Reading routing results")
+    print("Reading routing results...")
     return pl.read_parquet(os.path.join(tmp_directory, "output", "ea_results.parquet"))
 
 
 def merge(trips: pl.DataFrame, queries: pl.DataFrame, results: pl.DataFrame):
-    print("Merging trips data")
+    print("Merging trips data...")
     results = results.join(queries, on="query_id").select(
-        "origin", "destination", pl.col("arrival_time").alias("distance")
+        "origin", "destination", pl.col("arrival_time").alias("distance_network")
     )
     trips = trips.join(
         results,
@@ -177,8 +216,77 @@ def merge(trips: pl.DataFrame, queries: pl.DataFrame, results: pl.DataFrame):
         right_on=["origin", "destination"],
         how="left",
     )
-    print("Walking distance:\n{}".format(trips["distance"].describe()))
+    print("Walking distance (network):\n{}".format(trips["distance_network"].describe()))
+    trips = trips.with_columns(
+        (
+            pl.col("distance_network")
+            + pl.col("origin_node_dist")
+            + pl.col("destination_node_dist")
+        ).alias("distance")
+    )
+    print("Walking distance (total):\n{}".format(trips["distance"].describe()))
     return trips
+
+
+def plot_variables(trips: pl.DataFrame, graph_dir: str):
+    print("Generating graphs of the variables")
+    if not os.path.isdir(graph_dir):
+        os.makedirs(graph_dir)
+    # Distance between origin and start node.
+    fig, ax = mpl.get_figure(fraction=0.8)
+    m = max(0.0, np.log(trips.select("origin_node_dist").min().item()))
+    bins = (
+        np.logspace(m, np.log1p(trips.select("origin_node_dist").max().item()), 50, base=np.e)
+        - 1.0
+    )
+    ax.hist(trips["origin_node_dist"], bins=bins, color=mpl.CMP(0))
+    ax.set_xlabel("Distance origin / start node (meters)")
+    ax.set_xscale("log")
+    ax.set_ylabel("Count")
+    fig.tight_layout()
+    fig.savefig(os.path.join(graph_dir, "origin_node_dist_distribution.pdf"))
+    # Distance between destination and end node.
+    fig, ax = mpl.get_figure(fraction=0.8)
+    m = max(0.0, np.log(trips.select("destination_node_dist").min().item()))
+    bins = (
+        np.logspace(
+            m, np.log1p(trips.select("destination_node_dist").max().item()), 50, base=np.e
+        )
+        - 1.0
+    )
+    ax.hist(trips["destination_node_dist"], bins=bins, color=mpl.CMP(0))
+    ax.set_xlabel("Distance destination / end node (meters)")
+    ax.set_xscale("log")
+    ax.set_ylabel("Count")
+    fig.tight_layout()
+    fig.savefig(os.path.join(graph_dir, "destination_node_dist_distribution.pdf"))
+    # Walking distance network.
+    fig, ax = mpl.get_figure(fraction=0.8)
+    m = max(0.0, np.log(trips.select("distance_network").min().item()))
+    bins = (
+        np.logspace(m, np.log1p(trips.select("distance_network").max().item()), 50, base=np.e)
+        - 1.0
+    )
+    ax.hist(
+        trips.filter(pl.col("distance_network") > 0)["distance_network"],
+        bins=bins,
+        color=mpl.CMP(1),
+    )
+    ax.set_xlabel("Walking distance on network (meters)")
+    ax.set_xscale("log")
+    ax.set_ylabel("Count")
+    fig.tight_layout()
+    fig.savefig(os.path.join(graph_dir, "walking_distance_network_distribution.pdf"))
+    # Walking distance.
+    fig, ax = mpl.get_figure(fraction=0.8)
+    m = max(0.0, np.log(trips.select("distance").min().item()))
+    bins = np.logspace(m, np.log1p(trips.select("distance").max().item()), 50, base=np.e) - 1.0
+    ax.hist(trips.filter(pl.col("distance") > 0)["distance"], bins=bins, color=mpl.CMP(1))
+    ax.set_xlabel("Walking distance (meters)")
+    ax.set_xscale("log")
+    ax.set_ylabel("Count")
+    fig.tight_layout()
+    fig.savefig(os.path.join(graph_dir, "walking_distance_distribution.pdf"))
 
 
 if __name__ == "__main__":
@@ -201,26 +309,40 @@ if __name__ == "__main__":
 
     t0 = time.time()
 
-    trips = read_trips(config["population_directory"])
+    trips, nodes = read_trips(config["population_directory"])
 
     edges = read_edges(
         config["clean_walk_edges_file"], config["crs"], config["routing"]["walking_distance"]
     )
 
-    df = find_origin_destination_node(trips, edges)
+    df = find_origin_destination_node(nodes, edges)
 
-    queries = prepare_routing(df, edges, config["tmp_directory"])
+    trips = add_od_to_trips(trips, df)
+
+    queries = prepare_routing(trips, edges, config["tmp_directory"])
 
     run_routing(config["routing_exec"], config["tmp_directory"])
 
-    results = load_results(config["tmp_directry"])
+    results = load_results(config["tmp_directory"])
 
-    df = merge(df, queries, results)
+    trips = merge(trips, queries, results)
 
     metro_io.save_dataframe(
-        df,
+        trips,
         config["routing"]["walking_distance"]["output_filename"],
     )
+
+    if config["routing"]["walking_disance"].get("output_graphs", False):
+        if not "graph_directory" in config:
+            raise Exception("Missing key `graph_directory` in config")
+        graph_dir = os.path.join(config["graph_directory"], "routing.walking_distance")
+        plot_variables(trips, graph_dir)
+
+    # Clean the temporary directory.
+    try:
+        shutil.rmtree(config["tmp_directory"])
+    except OSError as e:
+        print(e)
 
     t = time.time() - t0
     print("Total running time: {:.2f} seconds".format(t))
