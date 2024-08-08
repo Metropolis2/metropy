44e5e3bea3c22bffff9b137b62f9f8c7198fc584
LucasJavaudin <lucas@lucasjavaudin.com>
2024-08-08 15:47:47 +0200

    Fix gitignore

---
+++ metropy/run/base.py
@@ -0,0 +1,151 @@
+# Base functions used by multiple scripts to generate simulation input.
+import os
+
+import polars as pl
+
+import metropy.utils.io as metro_io
+
+
+# Base parameters, common to all simulations.
+PARAMETERS = {
+    "input_files": {
+        "agents": "input/agents.parquet",
+        "alternatives": "input/alts.parquet",
+        "trips": "input/trips.parquet",
+        "edges": "input/edges.parquet",
+        "vehicle_types": "input/vehicles.parquet",
+    },
+    "output_directory": "output",
+    "learning_model": {
+        "type": "Exponential",
+    },
+    "road_network": dict(),
+    "saving_format": "Parquet",
+}
+
+
+def read_edges(
+    edge_filename: str, edge_main_filename: None | str, edge_penalties_filename: None | str
+):
+    print("Reading edges")
+    edges = metro_io.scan_dataframe(edge_filename)
+    if edge_main_filename is not None:
+        edges_main = metro_io.scan_dataframe(edge_main_filename).filter("main")
+        edges = edges.join(edges_main, on=pl.col("edge_id"), how="semi")
+    if edge_penalties_filename is not None:
+        edges_penalties = metro_io.scan_dataframe(edge_penalties_filename).rename(
+            {"additive_penalty": "constant_travel_time"}
+        )
+        columns = edges_penalties.collect_schema().names()
+        assert (
+            "constant_travel_time" in columns
+        ), "No column `additive_penalty` in the edges penalties file"
+        if "speed" in columns:
+            # Drop the speed column and join to replace it with the penalty speed.
+            edges = (
+                edges.rename({"speed": "base_speed"})
+                .join(edges_penalties, on="edge_id", how="left")
+                .with_columns(
+                    pl.col("constant_travel_time").fill_null(pl.lit(0.0)),
+                    pl.col("speed").fill_null(pl.col("base_speed")),
+                )
+            )
+        else:
+            print("Warning: no `speed` column in the edges penalties, using additive penalty only")
+            edges = edges.join(edges_penalties, on="edge_id", how="left").with_columns(
+                pl.col("constant_travel_time").fill_null(pl.lit(0.0)),
+            )
+    else:
+        edges = edges.with_columns(constant_travel_time=pl.lit(0.0))
+    return edges
+
+
+def generate_road_network(edges: pl.LazyFrame, config: dict):
+    print("Creating Metropolis road network")
+    vehicles = [
+        {
+            "vehicle_id": 1,
+            "headway": config.get("car_headway", 10.0) / config.get("simulation_ratio", 1.0),
+            "pce": config.get("car_pce", 1.0) / config.get("simulation_ratio", 1.0),
+        },
+    ]
+    vehicles = pl.DataFrame(vehicles)
+    # Convert edges' speed from km/h to m/s.
+    edges = edges.with_columns(pl.col("speed") / 3.6)
+    edges = edges.with_columns(pl.lit(config.get("overtaking", True)).alias("overtaking"))
+    columns = [
+        "edge_id",
+        "source",
+        "target",
+        "speed",
+        "length",
+        "lanes",
+        "overtaking",
+        "constant_travel_time",
+    ]
+    sort_columns = ["lanes", "speed", "length"]
+    sort_descending = [True, True, False]
+    if config.get("use_bottleneck", False):
+        assert isinstance(
+            config["edge_capacity"], dict
+        ), "Edge capacities must be specified when `use_bottleneck=true`."
+        edges = edges.with_columns(
+            bottleneck_flow=pl.col("road_type").replace_strict(
+                config["edge_capacity"], return_dtype=pl.Float64
+            )
+            / 3600
+        )
+        columns.append("bottleneck_flow")
+        sort_columns.insert(0, "bottleneck_flow")
+        sort_descending.insert(0, True)
+    edges_df = edges.select(columns).collect()
+    # Remove parallel edges.
+    n0 = len(edges_df)
+    edges_df = edges_df.sort(sort_columns, descending=sort_descending).unique(
+        subset=["source", "target"], keep="first"
+    )
+    n1 = len(edges_df)
+    if n0 > n1:
+        print("Warning: Discarded {:,} parallel edges".format(n0 - n1))
+    edges_df = edges_df.sort("source")
+    return edges_df, vehicles
+
+
+def read_trips(population_directory: str, car_split_filename: str, period: list[float]):
+    print("Reading trips")
+    trips = metro_io.scan_dataframe(os.path.join(population_directory, "trips.parquet")).rename(
+        {"person_id": "agent_id"}
+    )
+    # Remove trips with origin = destination.
+    trips = trips.filter(
+        (pl.col("origin_lng") != pl.col("destination_lng"))
+        | (pl.col("origin_lat") != pl.col("destination_lat"))
+    )
+    # Remove trips outside of the simulation period.
+    trips = trips.filter(pl.col("departure_time").is_between(period[0], period[1]))
+    car_split = metro_io.scan_dataframe(car_split_filename)
+    trips = trips.join(car_split, on="trip_id", how="left", coalesce=True)
+    trip_modes = metro_io.scan_dataframe(
+        os.path.join(population_directory, "trip_modes.parquet")
+    ).filter(pl.col("mode") == "car_driver")
+    trips = trips.join(trip_modes, on="trip_id", how="semi")
+    trips = trips.sort("agent_id")
+    return trips
+
+
+def write_agents(run_directory: str, agents: pl.DataFrame, alts: pl.DataFrame, trips: pl.DataFrame):
+    print("Writing agents")
+    agents.write_parquet(os.path.join(run_directory, "input", "agents.parquet"), use_pyarrow=True)
+    print("Writing alternatives")
+    alts.write_parquet(os.path.join(run_directory, "input", "alts.parquet"), use_pyarrow=True)
+    print("Writing trips")
+    trips.write_parquet(os.path.join(run_directory, "input", "trips.parquet"), use_pyarrow=True)
+
+
+def write_road_network(run_directory: str, edges: pl.DataFrame, vehicles: pl.DataFrame):
+    print("Writing edges")
+    edges.write_parquet(os.path.join(run_directory, "input", "edges.parquet"), use_pyarrow=True)
+    print("Writing vehicle types")
+    vehicles.write_parquet(
+        os.path.join(run_directory, "input", "vehicles.parquet"), use_pyarrow=True
+    )
---
+++ metropy/run/tomtom_routes.py
@@ -0,0 +1,166 @@
+import os
+import json
+
+import polars as pl
+
+import metropy.utils.io as metro_io
+import metropy.run.base as base_functions
+
+
+def read_tomtom_paths(input_file: str, edges: pl.DataFrame, period: list[float]):
+    print("Reading TomTom paths...")
+    lf = metro_io.scan_dataframe(input_file)
+    # Filter-out trips during the weekends.
+    lf = lf.filter(pl.col("departure_time").dt.weekday() <= 5)
+    # Create departure time column (in seconds after midnight).
+    lf = lf.with_columns(
+        td=(
+            pl.col("departure_time").dt.hour().cast(pl.UInt64) * 3600
+            + pl.col("departure_time").dt.minute().cast(pl.UInt64) * 60
+            + pl.col("departure_time").dt.second().cast(pl.UInt64)
+        ).cast(pl.Float64)
+    )
+    # Filter-out trips outside of the period.
+    lf = lf.filter(pl.col("td") >= period[0], pl.col("td") <= period[1])
+    # Filter-out paths where some edges are not part of the road network (probably the parallel
+    # edges).
+    edge_ids = set(edges["edge_id"])
+    lf = lf.filter(pl.col("cpath").list.eval(pl.element().is_in(edge_ids)).list.all())
+    # Find origin and destination nodes.
+    lf = lf.with_columns(
+        pl.col("cpath").list.first().replace_strict(edges["edge_id"], edges["source"]),
+        pl.col("cpath").list.last().replace_strict(edges["edge_id"], edges["target"]),
+    )
+    df = lf.select("id", "cpath", "dt").collect()
+    print("Number of paths read: {:,}".format(len(df)))
+    return df
+
+
+def read_trips(population_directory: str, car_split_filename: str, period: list[float]):
+    print("Reading trips")
+    trips = metro_io.scan_dataframe(os.path.join(population_directory, "trips.parquet")).rename(
+        {"person_id": "agent_id"}
+    )
+    # Remove trips with origin = destination.
+    trips = trips.filter(
+        (pl.col("origin_lng") != pl.col("destination_lng"))
+        | (pl.col("origin_lat") != pl.col("destination_lat"))
+    )
+    # Remove trips outside of the simulation period.
+    trips = trips.filter(pl.col("departure_time").is_between(period[0], period[1]))
+    car_split = metro_io.scan_dataframe(car_split_filename)
+    trips = trips.join(car_split, on="trip_id", how="left", coalesce=True)
+    trip_modes = metro_io.scan_dataframe(
+        os.path.join(population_directory, "trip_modes.parquet")
+    ).filter(pl.col("mode") == "car_driver")
+    trips = trips.join(trip_modes, on="trip_id", how="semi")
+    trips = trips.sort("agent_id")
+    return trips
+
+
+def generate_agents(tomtom: pl.DataFrame):
+    tomtom = tomtom.with_columns(agent_id=pl.col("id"))
+    assert len(tomtom) == tomtom["agent_id"].n_unique(), "Id should be unique over TomTom requests"
+    print("Creating agent-level DataFrame")
+    # Agent-level values: only `agent_id` is useful.
+    agents = tomtom.select("agent_id")
+
+    print("Creating alternative-level DataFrame")
+    # Alternative-level values.
+    alts = tomtom.select(
+        "agent_id",
+        pl.lit(1).alias("alt_id"),
+        pl.lit("Constant").alias("dt_choice.type"),
+        pl.col("td").alias("dt_choice.departure_time"),
+    )
+
+    print("Creating trip-level DataFrame")
+    # Trip-level values.
+    trips = tomtom.select(
+        "agent_id",
+        pl.lit(1).alias("alt_id"),
+        pl.col("agent_id").alias("trip_id"),
+        pl.lit("Road").alias("class.type"),
+        pl.col("origin").alias("class.origin"),
+        pl.col("destination").alias("class.destination"),
+        pl.lit(1).alias("class.vehicle"),
+    )
+
+    print(
+        "Generated {:,} agents, with {:,} alternatives and {:,} trips".format(
+            agents.height,
+            alts.height,
+            trips.height,
+        )
+    )
+    nb_origins = trips["class.origin"].n_unique()
+    print("Number of unique origins: {}".format(nb_origins))
+    nb_destinations = trips["class.destination"].n_unique()
+    print("Number of unique destinations: {}".format(nb_destinations))
+    return agents, alts, trips
+
+
+def write_parameters(run_directory: str, config: dict, car_only_directory: str):
+    parameters = base_functions.PARAMETERS.copy()
+    parameters["input_files"]["road_network_conditions"] = os.path.join(
+        car_only_directory, "output", "net_cond_next_exp_edge_ttfs.parquet"
+    )
+    parameters["period"] = config["period"]
+    parameters["road_network"]["recording_interval"] = config["recording_interval"]
+    parameters["road_network"]["spillback"] = config["spillback"]
+    parameters["road_network"]["max_pending_duration"] = config["max_pending_duration"]
+    if "backward_wave_speed" in config:
+        parameters["road_network"]["backward_wave_speed"] = config["backward_wave_speed"]
+    parameters["road_network"]["algorithm_type"] = config["routing_algorithm"]
+    parameters["only_compute_decisions"] = True
+    print("Writing parameters")
+    with open(os.path.join(run_directory, "parameters.json"), "w") as f:
+        f.write(json.dumps(parameters))
+
+
+if __name__ == "__main__":
+    from metropy.config import read_config, check_keys
+
+    config = read_config()
+    mandatory_keys = [
+        "crs",
+        "clean_edges_file",
+        "population_directory",
+        "calibration.post_map_matching.output_filename",
+        "run.edge_capacity",
+        "run.period",
+        "run.recording_interval",
+        "run.spillback",
+        "run.max_pending_duration",
+        "run.routing_algorithm",
+        "run.car_only.directory",
+        "run.tomtom_routes.directory",
+    ]
+    check_keys(config, mandatory_keys)
+
+    run_directory = config["run"]["tomtom_routes"]["directory"]
+    if not os.path.isdir(os.path.join(run_directory, "input")):
+        os.makedirs(os.path.join(run_directory, "input"))
+
+    # Read edges without the main file so that all edges are included (except the parallel ones).
+    edges = base_functions.read_edges(
+        config["clean_edges_file"],
+        None,
+        config["edge_penalties_file"],
+    )
+    edges, vehicles = base_functions.generate_road_network(edges, config["run"])
+
+    tomtom = read_tomtom_paths(
+        config["calibration"]["post_map_matching"]["output_filename"],
+        edges,
+        config["run"]["period"],
+    )
+    agents, alts, trips = generate_agents(tomtom)
+
+    base_functions.write_agents(run_directory, agents, alts, trips)
+
+    base_functions.write_road_network(run_directory, edges, vehicles)
+
+    write_parameters(run_directory, config["run"],
+     config["run"]["car_only"]["directory"]
+                     )
